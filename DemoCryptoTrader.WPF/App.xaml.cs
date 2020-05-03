fb7f9925e38ebcf033ba5fa122b9c3a412fcbbb7
Alienware <Alienware@DESKTOP-L05G1MH>
2020-05-02 21:34:38 -0400

    Basic WPF UI skeleton

--- DemoCryptoTrader.WPF/App.xaml.cs
+++ DemoCryptoTrader.WPF/App.xaml.cs
@@ -13,5 +13,13 @@ namespace DemoCryptoTrader.WPF
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
