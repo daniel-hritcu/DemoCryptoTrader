f66ab086985fdb426d8c735a6c341e4e08f4dd47
Daniel Hritcu <daniel@hritcu.ca>
2020-05-03 16:40:22 -0400

    add CoinIndexService

---
+++ DemoCryptoTrader.Domain/Models/CoinIndex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCryptoTrader.Domain.Models
+{
+    public enum CoinIndexId
+    { 
+        bitcoin,
+        etherium
+    }
+
+    public class CoinIndex
+    {
+        public double Price { get; set; }
+        public string Changes { get; set; }
+        public CoinIndexId Id { get; set; }
+
+    }
+}
---
+++ DemoCryptoTrader.Domain/Services/ICoinIndexService.cs
@@ -0,0 +1,13 @@
+﻿using DemoCryptoTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCryptoTrader.Domain.Services
+{
+    public interface ICoinIndexService
+    {
+        Task<CoinIndex> GetCoinIndex(CoinIndexId indexId);
+    }
+}
