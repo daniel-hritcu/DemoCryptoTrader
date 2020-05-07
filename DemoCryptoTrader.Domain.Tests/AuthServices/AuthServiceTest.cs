bfe30f05250bf73089b39d88defc125b89ea5903
Daniel Hritcu <daniel@hritcu.ca>
2020-05-06 20:44:11 -0400

    UnitTest Account

---
+++ DemoCryptoTrader.Domain.Tests/AuthServices/AuthServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Moq;
+using NUnit.Framework;
+using DemoCryptoTrader.Domain.Exceptions;
+using DemoCryptoTrader.Domain.Models;
+using DemoCryptoTrader.Domain.Services;
+using DemoCryptoTrader.Domain.Services.AuthServices;
+
+
+
+namespace DemoCryptoTrader.Domain.Tests.AuthServices
+{
+    [TestFixture]
+    public class AuthServiceTest
+    {
+        private Mock<IPasswordHasher> _mockPasswordHasher;
+        private Mock<IAccountService> _mockAccountService;
+        private AuthService _authService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockPasswordHasher = new Mock<IPasswordHasher>();
+            _mockAccountService = new Mock<IAccountService>();
+            _authService = new AuthService(_mockAccountService.Object, _mockPasswordHasher.Object);
+        }
+
+        [Test]
+        public void Login_WithIncorrectPasswordForExistingUsername_ThrowsInvalidPasswordExceptionForUsername()
+        {
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            _mockAccountService.Setup(s => s.GetByUsername(expectedUsername)).ReturnsAsync(new Account() { AccountHolder = new User() { Username = expectedUsername } });
+            _mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Failed);
+
+            InvalidPasswordException exception = Assert.ThrowsAsync<InvalidPasswordException>(() => _authService.Login(expectedUsername, password));
+
+            string actualUsername = exception.Username;
+            Assert.AreEqual(expectedUsername, actualUsername);
+        }
+
+        [Test]
+        public void Login_WithNonExistingUsername_ThrowsInvalidPasswordExceptionForUsername()
+        {
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            _mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Failed);
+
+            UserNotFoundException exception = Assert.ThrowsAsync<UserNotFoundException>(() => _authService.Login(expectedUsername, password));
+
+            string actualUsername = exception.Username;
+            Assert.AreEqual(expectedUsername, actualUsername);
+        }
+    }
+}
